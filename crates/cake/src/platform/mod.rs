1d33ca870ccdddb70350402200a08a14b99bf661
joshua <jyou12@berkeley.edu>
2025-01-05 02:47:39 -0800

    fix typo in platform.rs

--- crates/cake/src/platform/mod.rs
+++ crates/cake/src/platform/mod.rs
@@ -1,7 +1,7 @@
 use std::path::{Path, PathBuf};
 
-pub struct Platform {
-    pub(crate) working_dir: PathBuf,
+pub(crate) struct Platform {
+    working_dir: PathBuf,
     system_header_paths: Vec<PathBuf>,
     normal_header_paths: Vec<PathBuf>,
 }
@@ -15,6 +15,14 @@ impl Platform {
         }
     }
 
+    pub(crate) fn working_dir(&self) -> &PathBuf {
+        &self.working_dir
+    }
+
+    // resolves #include directives which use quotation marks
+    // first look in working directory, then in normal_header_paths
+    // to match gcc/clang behavior, these would be system paths like /usr/include as well as
+    // any additional include paths specified using -I
     pub(crate) fn resolve_normal_include_path(&self, normal: &str) -> Option<PathBuf> {
         let working_dir_path = self.working_dir.join(normal);
         if Path::exists(&working_dir_path) {
@@ -31,13 +39,10 @@ impl Platform {
         None
     }
 
+    // resolves #include directives which use angle brackets
+    // does not look in working directory first
     pub(crate) fn resolve_system_include_path(&self, system: &str) -> Option<PathBuf> {
-        let working_dir_path = self.working_dir.join(system);
-        if Path::exists(&working_dir_path) {
-            return Some(working_dir_path);
-        }
-
-        for system_path in &self.normal_header_paths {
+        for system_path in &self.system_header_paths {
             let path = system_path.join(system);
             if Path::exists(&path) {
                 return Some(path);
