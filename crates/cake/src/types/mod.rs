396809e3b09a84cb7a53ab88bbc34c6c62ea8b3f
joshua <jyou12@berkeley.edu>
2025-06-10 22:52:49 -0700

    prepare for major type refactor

--- crates/cake/src/types/mod.rs
+++ crates/cake/src/types/mod.rs
@@ -54,6 +54,7 @@ impl BasicType {
         }
     }
 
+    // we will only compile in LP64 model for now
     pub(crate) fn bytes(&self) -> u32 {
         match self {
             BasicType::Char => 1,
@@ -69,64 +70,144 @@ impl BasicType {
         }
     }
 
-    pub(crate) fn align_log2(&self) -> u8 {
-        self.bytes().ilog2() as u8
+    pub(crate) fn align(&self) -> u32 {
+        self.bytes()
     }
+}
+
+macro_rules! make_type_idx {
+    () => {};
 }
 
 // for now, all enums will be 4 bytes
 pub(crate) type EnumVariant = (String, i32);
+pub(crate) struct EnumType {
+    complete: bool,
+    tag: Option<String>,
+    members: Vec<EnumVariant>,
+}
 
-bitflags! {
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-    pub(crate) struct TypeQualifier: u8 {
-        const Const = 1;
-        const Restrict = 1 << 1;
-        const Volatile = 1 << 2;
+impl EnumType {
+    pub(crate) fn new_complete_enum_type(
+        tag: Option<String>,
+        members: Vec<EnumVariant>,
+    ) -> EnumType {
+        EnumType {
+            complete: true,
+            tag,
+            members,
+        }
+    }
+
+    pub(crate) fn new_incomplete_enum_type(tag: String) -> EnumType {
+        EnumType {
+            complete: false,
+            tag: Some(tag),
+            members: Vec::new(),
+        }
+    }
+
+    pub(crate) fn is_complete(&self) -> bool {
+        self.complete
+    }
+
+    pub(crate) fn members(&self) -> &[EnumVariant] {
+        &self.members
     }
 }
 
-// For now, not used
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub(crate) type AggregateMember = (String, QualifiedType);
+
+#[derive(Debug)]
+pub(crate) struct StructureType {
+    complete: bool,
+    tag: Option<String>,
+    members: Vec<AggregateMember>,
+}
+
+impl StructureType {
+    pub(crate) fn new_complete_structure_type(
+        tag: Option<String>,
+        members: Vec<AggregateMember>,
+    ) -> StructureType {
+        StructureType {
+            complete: true,
+            tag,
+            members,
+        }
+    }
+
+    pub(crate) fn new_incomplete_structure_type(tag: String) -> StructureType {
+        StructureType {
+            complete: false,
+            tag: Some(tag),
+            members: Vec::new(),
+        }
+    }
+
+    pub(crate) fn is_complete(&self) -> bool {
+        self.complete
+    }
+
+    pub(crate) fn members(&self) -> &[AggregateMember] {
+        &self.members
+    }
+}
+
+pub(crate) struct UnionType {
+    complete: bool,
+    tag: Option<String>,
+    members: Vec<AggregateMember>,
+}
+
+impl UnionType {
+    pub(crate) fn new_complete_union_type(
+        tag: Option<String>,
+        members: Vec<AggregateMember>,
+    ) -> UnionType {
+        UnionType {
+            complete: true,
+            tag,
+            members,
+        }
+    }
+
+    pub(crate) fn new_incomplete_union_type(tag: String) -> UnionType {
+        UnionType {
+            complete: false,
+            tag: Some(tag),
+            members: Vec::new(),
+        }
+    }
+
+    pub(crate) fn is_complete(&self) -> bool {
+        self.complete
+    }
+
+    pub(crate) fn members(&self) -> &[AggregateMember] {
+        &self.members
+    }
+}
+
+pub(crate) type FunctionArgument = (Option<String>, QualifiedType);
+
+// For now, not actually used to inform codegen (this is perfectly compliant with standard)
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub(crate) enum FunctionSpecifier {
     Inline,
     None,
 }
 
-pub(crate) type AggregateMember = (String, QualifiedType);
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub(crate) struct QualifiedType {
-    pub(crate) base_type: CType,
-    pub(crate) qualifier: TypeQualifier,
-}
-
-pub(crate) type FunctionArgument = (Option<String>, QualifiedType);
-
 #[derive(Clone, Debug, PartialEq)]
-pub(crate) struct FunctionTypeInner {
+pub(crate) struct FunctionType {
+    // public visibility of fields is reasonable, no real internal invariants to uphold
     pub(crate) parameter_types: Vec<FunctionArgument>,
     pub(crate) return_type: Box<QualifiedType>,
     pub(crate) function_specifier: FunctionSpecifier,
     pub(crate) varargs: bool,
+
+    // TODO: remove this from FunctionType, it should essentially be a parsing detail
     pub(crate) prototype_scope: Scope,
-}
-
-impl FunctionTypeInner {
-    // are two function types compatible?
-    fn compatible(a: &Self, b: &Self) -> bool {
-        // 1. return types are compatible
-        todo!();
-
-        // 2. parameter types are compatible
-        todo!();
-
-        // 3. whether they have varargs must be same
-        if a.varargs != b.varargs {
-            return false;
-        }
-
-        todo!()
-    }
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -154,7 +235,22 @@ pub(crate) enum CanonicalType {
         tag: Option<String>,
         members: Vec<EnumVariant>,
     },
-    FunctionType(FunctionTypeInner),
+    FunctionType(FunctionType),
+}
+
+bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    pub(crate) struct TypeQualifier: u8 {
+        const Const = 1;
+        const Restrict = 1 << 1;
+        const Volatile = 1 << 2;
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub(crate) struct QualifiedType {
+    pub(crate) base_type: CType,
+    pub(crate) qualifier: TypeQualifier,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
