cf6cd8f19aa0481ed10035443b1fbb6cc47d49f5
joshua <jyou12@berkeley.edu>
2025-10-24 13:53:36 -0700

    multidimensional array test

--- crates/cake/src/codegen/cranelift_backend/tests.rs
+++ crates/cake/src/codegen/cranelift_backend/tests.rs
@@ -783,6 +783,34 @@ fn test_union_punning() {
 }
 
 #[test]
+fn test_multi_array() {
+    // test multi-dimensional arrays and pointer-to-array arithmetic
+    // as well as layout (should be row-major)
+    let code = r#"
+    int main() {
+        int arr[2][3];
+        arr[0][0] = 1;
+        arr[0][1] = 2;
+        arr[0][2] = 3;
+
+        int *pointer_to_first[3] = &arr[0];
+        int *pointer_to_second[3] = pointer_to_first + 1;
+        pointer_to_second[0] = 4;
+
+        int *flattened = &arr[0][0];
+        int sum = 0;
+        for (int i = 0; i < 6; i++) {
+            sum += flattened[i];
+        }
+
+        return sum;
+    }
+    "#;
+
+    test_harness("multi_array", code, "", 0);
+}
+
+#[test]
 fn test_sizeof() {
     let code = r#"
     struct data {
