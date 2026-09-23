bbd38f8c61622f62a6178fcf77b227aef05837c0
joshua <jyou12@berkeley.edu>
2026-09-23 10:09:38 -0700

    work on regalloc2 integration to learn what the interface might look like

--- crates/cake/src/regalloc/basic_alloc.rs
+++ crates/cake/src/regalloc/basic_alloc.rs
@@ -1,6 +1,7 @@
 //! Basic Register Allocator
 //! 
-//! This is the most basic register allocator possible, as it just spills every value onto the stack.
+//! This is the most basic register allocator possible. Between basic blocks, everything is spilled to stack.
+//! Within a single basic block, it just does linear scan allocation
 //! It is used for testing and to bring up the register allocator interface
 
 use crate::regalloc::RegisterAllocator;
--- crates/cake/src/regalloc/mod.rs
+++ crates/cake/src/regalloc/mod.rs
@@ -1,14 +1,27 @@
 //! Register allocator interface. Uses a pull-based approach to query the IR through a set of traits,
 //! and a push-based approach to apply edits to the IR at the end
 
-pub(crate) trait Function {
-    
-}
+use cake_util::{IndexVec, make_type_idx};
+
+use crate::cir::Type;
+
+pub(crate) struct VReg(u32);
+pub(crate) struct PReg(u32);
+
+pub(crate) struct ProgramPoint(u32);
 
 pub(crate) enum Edit {
-    
+
 }
 
+pub(crate) struct SpillSlot(Type);
+make_type_idx!(SpillSlotRef, SpillSlot);
+
+
+pub(crate) struct RegisterAllocatorOutput {
+    spill_slots: IndexVec<SpillSlotRef, SpillSlot>,
+    
+}
 
 pub(crate) trait RegisterAllocator {
     fn allocate(&mut self);
